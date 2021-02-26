5d8835b4ce9aeeefbd0bb774e87aaf33f1eb8962
sahil <sahilofficial17@gmail.com>
2021-02-26 21:12:31 +0530

    v1.0

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ const appid = '2fc7f2a0800958ccdd66788bde858aa5';
 $(document).ready(function (){
     
     $('button').click(function (){
-        $('.card').toggle();
         var a = $('#search-input').val();
         var url = `https://api.openweathermap.org/data/2.5/weather?q=${a}&units=metric&appid=${appid}`
         $.getJSON(url, function (data){
+            $('.card').show();
+            $('.alert').hide();
             console.log(data);
             var iconcode = data.weather[0].icon;
             var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
@@ -16,7 +17,13 @@ $(document).ready(function (){
             $('#humidity').html(data.main.humidity);
             $('#pressure').html(data.main.pressure);
             $('#desc').html(data.weather[0].main);
-        });
+        }).done(function() {
+            console.log( " success" );
+          })
+          .fail(function() {
+            $('.alert').show();
+            $('.card').hide();
+          });
         
     });
 });
